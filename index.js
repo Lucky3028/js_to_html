35c5adb76e49a474b08ddff5c1473e136ff7412d
Lucky3028 <lucky3028.0@gmail.com>
2020-12-04 15:23:33 +0900

    [Fix] 命名規則に従って変数や関数を変更

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // ファイルの変更通知をしてくれるモジュール
-const chokidar = require("chokidar");
+const CHOKIDAR = require("chokidar");
 // ファイルシステムモジュール
-const fs = require('fs');
+const FILE_SYSTEM = require('fs');
 // 色を付けるためのモジュール
 require('colors');
 
 // chokidarの初期化
-const watcher = chokidar.watch('./scripts/',{
-  ignored:/[\/\\]\./,
+const WATCHER = CHOKIDAR.watch('./scripts/', {
+  ignored: /[\/\\]\./,
   persistent: true
 });
 
@@ -15,18 +15,18 @@ const watcher = chokidar.watch('./scripts/',{
 const SRC_HTML = 'template.html';
 
 // HH:MM:SS のかたちで現在時刻を取得。1桁の場合0埋め
-function getDate() {
-  const now = new Date();
-  return (("00" + now.getHours()).slice(-2) + ':' +
-            ("00" + now.getMinutes()).slice(-2) + ':' +
-            ("00" + now.getSeconds()).slice(-2)).gray;
+function getTime() {
+  const NOW_DATE = new Date();
+  return (("00" + NOW_DATE.getHours()).slice(-2) + ':' +
+    ("00" + NOW_DATE.getMinutes()).slice(-2) + ':' +
+    ("00" + NOW_DATE.getSeconds()).slice(-2)).gray;
 }
 
 function getFileName(path) {
   // pathをフォルダ階層ごとに分離する
-  const splittedPath = path.split('\\');
+  const SPLITTED_PATH = path.split('\\');
   // ファイル名のみにして拡張子を削除する
-  return splittedPath[splittedPath.length - 1].replace('.js', "");
+  return SPLITTED_PATH[SPLITTED_PATH.length - 1].replace('.js', "");
 }
 
 function isJavaScriptFile(path) {
@@ -39,41 +39,41 @@ function generateHtmlFileName(path) {
 }
 
 // イベント定義
-watcher.on('ready',function(){
+WATCHER.on('ready', function () {
 
-    console.log(`[${getDate()}] Ready watching...`);
+  console.log(`[${getTime()}] Ready watching...`);
 
-    // jsファイルが追加されたら
-    watcher.on('add',function(path){
-      if (!isJavaScriptFile(path)) return;
+  // jsファイルが追加されたら
+  WATCHER.on('add', function (path) {
+    if (!isJavaScriptFile(path)) return;
 
-      const dest = generateHtmlFileName(path);
+    const DEST_HTML = generateHtmlFileName(path);
 
-      // SRC_HTMLに指定されたhtmlのコピーを作成し（すでに存在するなら上書き）、追加されたjsと同じファイル名にリネーム
-      fs.copyFileSync(SRC_HTML, dest);
+    // SRC_HTMLに指定されたhtmlのコピーを作成し（すでに存在するなら上書き）、追加されたjsと同じファイル名にリネーム
+    FILE_SYSTEM.copyFileSync(SRC_HTML, DEST_HTML);
 
-      // html構造で指定されているjsを、追加されたjsに変更
-      let html = fs.readFileSync(dest, 'UTF-8');
-      html = html.replace('scripts/template.js', path);
-      fs.writeFileSync(dest, html);
+    // html構造で指定されているjsを、追加されたjsに変更
+    let html = FILE_SYSTEM.readFileSync(DEST_HTML, 'UTF-8');
+    html = html.replace('scripts/template.js', path);
+    FILE_SYSTEM.writeFileSync(DEST_HTML, html);
 
-      console.log(`[${getDate()}] '${dest.cyan}' added.`);
-    });
+    console.log(`[${getTime()}] '${DEST_HTML.cyan}' added.`);
+  });
 
-    // jsファイルが削除されたら
-    watcher.on('unlink', function (path) {
-      if (!isJavaScriptFile(path)) return;
+  // jsファイルが削除されたら
+  WATCHER.on('unlink', function (path) {
+    if (!isJavaScriptFile(path)) return;
 
-      const dest = generateHtmlFileName(path);
+    const DEST_HTML = generateHtmlFileName(path);
 
-      try {
-        // 同名のhtmlを削除
-        fs.unlinkSync(dest);
-        console.log(`[${getDate()}] '${dest.cyan}' removed.`);
-      } catch (err) {
-        if (err.code === 'ENOENT') {
-          console.error(`[${getDate()}] '${path.cyan}' was removed, but '${dest.cyan}' was not found.`)
-        }
+    try {
+      // 同名のhtmlを削除
+      FILE_SYSTEM.unlinkSync(DEST_HTML);
+      console.log(`[${getTime()}] '${DEST_HTML.cyan}' removed.`);
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.error(`[${getTime()}] '${path.cyan}' was removed, but '${DEST_HTML.cyan}' was not found.`)
       }
+    }
   });
 });
