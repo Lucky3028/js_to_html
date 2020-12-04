a636aa950f5d08ef86e86086a677319d3506d142
Lucky3028 <lucky3028.0@gmail.com>
2020-12-04 11:03:06 +0900

    [Remove] 不要なコメント

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// ref. 「node.jsでファイルの変更を検出して何かする」https://qiita.com/zaburo/items/0280807fe5e59026e41b
-//      「Node.jsで標準出力や標準エラー出力に色付きの文字列を出力する」http://info-i.net/console-log-color
-//      「node.jsでファイルの入出力操作」https://qiita.com/shirokuman/items/509b159bf4b8dd1c41ef
-//      「node.jsでファイルを削除する」https://www.gesource.jp/weblog/?p=8216
-//      「Node.js での例外処理: (1) try ～ catch による例外処理」https://maku77.github.io/nodejs/exception/try-and-catch.html
-
 // ファイルの変更通知をしてくれるモジュール
 const chokidar = require("chokidar");
 const { NOTFOUND } = require("dns");
